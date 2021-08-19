8a7bd85c2acd3eba11564e5a97978c81f6d1b527
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-08-19 09:30:06 -0500

    Cleanup sample code

--- src/evpn-proxy-agent/evpn-proxy-agent.py
+++ src/evpn-proxy-agent/evpn-proxy-agent.py
@@ -1,4 +1,5 @@
 import eventlet
+import signal
 
 # BGPSpeaker needs sockets patched
 eventlet.monkey_patch()
@@ -11,7 +12,11 @@ import sys
 log = logging.getLogger()
 log.addHandler(logging.StreamHandler(sys.stderr))
 
-from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker, EVPN_MULTICAST_ETAG_ROUTE, RF_L2_EVPN
+from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker,
+                                                  EVPN_MULTICAST_ETAG_ROUTE,
+                                                  EVPN_MAC_IP_ADV_ROUTE,
+                                                  RF_L2_EVPN,
+                                                  PMSI_TYPE_INGRESS_REP
 
 def dump_remote_best_path_change(event):
     print( 'the best path changed:', event.remote_as, event.prefix, event.nexthop, event.is_withdraw )
@@ -20,42 +25,66 @@ def detect_peer_down(remote_ip, remote_as):
     print( f'Peer down: {remote_ip} {remote_as}' )
 
 if __name__ == "__main__":
+
+    LOCAL_LOOPBACK = "1.1.1.4" # Local loopback interface on SRL
+    VTEP_LOOPBACK = "1.1.1.2"  # Remote VTEP we're impersonating
+    AS = 65000
+    VNI = 10
+
     # need to create socket on localhost on a non-default port, not port 179
-    speaker = BGPSpeaker(bgp_server_port=1179, as_number=65000, router_id='10.0.0.1',
+    # Need to connect from loopback IP
+    # Router ID is used as tunnel endpoint in BGP UPDATEs
+    speaker = BGPSpeaker(bgp_server_hosts=[LOCAL_LOOPBACK], bgp_server_port=1179,
+                         as_number=AS, router_id=VTEP_LOOPBACK,
                          best_path_change_handler=dump_remote_best_path_change,
                          peer_down_handler=detect_peer_down)
 
-    speaker.vrf_add(route_dist='65000:10',
-                    import_rts=['65000:10'],
-                    export_rts=['65000:10'],
-                    route_family=RF_L2_EVPN)
+    rd = f'{AS}:{VNI}'
+    speaker.vrf_add(route_dist=rd,import_rts=[rd],export_rts=[rd],route_family=RF_L2_EVPN)
 
     speaker.evpn_prefix_add(
         route_type=EVPN_MULTICAST_ETAG_ROUTE,
-        route_dist='65000:10',
-        esi=0,
+        route_dist=rd,
+        # esi=0, # should be ignored
         ethernet_tag_id=0,
-        # mac_addr=mac_addr,
-        # ip_addr=ip_addr,
-        next_hop='1.1.1.2', # on behalf of Cumulus
+        # mac_addr='00:11:22:33:44:55', # not relevant?
+        ip_addr=VTEP_LOOPBACK, # origin
+        tunnel_type='vxlan',
+        vni=VNI,
+        gw_ip_addr=VTEP_LOOPBACK,
+        next_hop=VTEP_LOOPBACK, # on behalf of remote VTEP
+        pmsi_tunnel_type=PMSI_TYPE_INGRESS_REP
     )
 
-    speaker.neighbor_add('127.0.0.1', 65000) # iBGP with SRL
-    count = 1
-    while True:
-        eventlet.sleep(30)
+    print( "Adding SRL neighbor..." )
+    # TODO enable_four_octet_as_number=True, enable_enhanced_refresh=True
+    speaker.neighbor_add(LOCAL_LOOPBACK, AS, enable_ipv4=False, enable_evpn=True, connect_mode='active') # iBGP with SRL
 
-        speaker.evpn_prefix_add(
-                route_type=EVPN_MULTICAST_ETAG_ROUTE,
-                route_dist='65000:10',
-                esi=0,
-                ethernet_tag_id=0,
-                mac_addr=f'00:11:22:33:44:0{count}',
-                ip_addr=f'10.0.0.{count}/32',
-                next_hop='1.1.1.2', # on behalf of Cumulus
-        )
+    print( "Adding new EVPN RT2 route..." )
+    speaker.evpn_prefix_add(
+        route_type=EVPN_MAC_IP_ADV_ROUTE, # RT2
+        route_dist=rd,
+        esi=0,
+        ethernet_tag_id=0,
+        mac_addr='00:11:22:33:44:55',
+        ip_addr='10.0.0.123',
+        next_hop=VTEP_LOOPBACK, # on behalf of remote VTEP
+        tunnel_type='vxlan',
+        vni=VNI,
+        gw_ip_addr=VTEP_LOOPBACK,
+        next_hop=VTEP_LOOPBACK, # on behalf of remote VTEP
+    )
 
-        count += 1
-        if count == 4:
-            speaker.shutdown()
-            break
+    def handler(signum, frame):
+       print( f"\nShutting down BGP instance...signal={signum}" )
+       speaker.neighbor_del(LOCAL_LOOPBACK)
+       speaker.shutdown()
+       system.exit(0)
+
+    # Register our handler for keyboard interrupt and termination signals
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+
+    # The process main thread does nothing but waiting for signals
+    print( "Pausing for interrupts..." )
+    signal.pause()
