798c1d58c02b489068beee5f353fcf18e069e5c3
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-04 16:37:06 -0500

    Fix bug in Ryu where path.source is set to VRF_TABLE

--- src/evpn-proxy-agent/evpn-proxy-agent.py
+++ src/evpn-proxy-agent/evpn-proxy-agent.py
@@ -600,7 +600,7 @@ if __name__ == '__main__':
     logging.basicConfig(
       handlers=[RotatingFileHandler(log_filename, maxBytes=3000000,backupCount=5)],
       format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
-      datefmt='%H:%M:%S', level=logging.INFO)
+      datefmt='%H:%M:%S', level=logging.DEBUG)
     logging.info("START TIME :: {}".format(datetime.datetime.now()))
     if Run():
         logging.info('Agent unregistered and BGP shutdown')
