9ee6e9056ac0a2dde147e5f9587dc00159138818
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-04 10:21:54 -0500

    Make local preference configurable

--- src/evpn-proxy-agent/evpn-proxy-agent.py
+++ src/evpn-proxy-agent/evpn-proxy-agent.py
@@ -205,7 +205,9 @@ def runBGPThread( state ):
      logging.info("Starting BGPSpeaker in netns...")
 
      speaker = BGPSpeaker(bgp_server_hosts=[LOCAL_LOOPBACK], bgp_server_port=1179,
-                               as_number=state.params['local_as'], router_id=LOCAL_LOOPBACK,
+                               as_number=state.params['local_as'],
+                               local_pref=state.params['local_preference'],
+                               router_id=LOCAL_LOOPBACK,
                                best_path_change_handler=best_path_change_handler,
                                peer_up_handler=peer_up_handler,
                                peer_down_handler=peer_down_handler)
@@ -427,6 +429,8 @@ def Handle_Notification(obj, state):
                     state.params[ "local_as" ] = int( data['local_as']['value'] )
                 if 'peer_as' in data:
                     state.params[ "peer_as" ] = int( data['peer_as']['value'] )
+                if 'local_preference' in data:
+                    state.params[ "local_preference" ] = int( data['local_preference']['value'] )
                 if 'source_address' in data:
                     state.params[ "source_address" ] = data['source_address']['value']
                 if 'peer_address' in data:
