e27d61cf3cfc80891be2fe83c35df7ace2737baf
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-23 13:45:38 -0500

    Add up to 5 pings

---
+++ labs/spine-leaf/test_mac_move.sh
@@ -0,0 +1,14 @@
+if ! docker exec -it clab-static-vxlan-spine-lab-h1 ping 10.0.0.103 -c2; then
+  echo "Ping h1->h3 failed!"
+  exit $?
+fi
+MAC1=`docker exec -it clab-static-vxlan-spine-lab-h1 ip a show dev eth1 | awk '/ether/{ print $2 }' | head -1`
+MAC2=`docker exec -it clab-static-vxlan-spine-lab-h2 ip a show dev eth1 | awk '/ether/{ print $2 }' | head -1`
+echo "MACs h1=${MAC1} h2=${MAC2}, swapping..."
+docker exec -it clab-static-vxlan-spine-lab-h1 ip link set address $MAC2 dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h1 ip addr flush dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h1 ip addr add 10.0.0.102/24 dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h2 ip link set address $MAC1 dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h2 ip addr flush dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h2 ip addr add 10.0.0.101/24 dev eth1
+docker exec -it clab-static-vxlan-spine-lab-h2 ping 10.0.0.103 -c5
