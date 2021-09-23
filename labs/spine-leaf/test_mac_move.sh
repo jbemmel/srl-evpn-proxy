a1b703671460cacda25b733aeadf9097b7e0c5fc
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-23 14:54:27 -0500

    Ping from both h1 and h2 afterwards

--- labs/spine-leaf/test_mac_move.sh
+++ labs/spine-leaf/test_mac_move.sh
@@ -11,4 +11,5 @@ docker exec -it clab-static-vxlan-spine-lab-h1 ip addr add 10.0.0.102/24 dev eth
 docker exec -it clab-static-vxlan-spine-lab-h2 ip link set address $MAC1 dev eth1
 docker exec -it clab-static-vxlan-spine-lab-h2 ip addr flush dev eth1
 docker exec -it clab-static-vxlan-spine-lab-h2 ip addr add 10.0.0.101/24 dev eth1
-docker exec -it clab-static-vxlan-spine-lab-h2 ping 10.0.0.103 -c5
+docker exec -it clab-static-vxlan-spine-lab-h1 ping 10.0.0.103 -c2
+docker exec -it clab-static-vxlan-spine-lab-h2 ping 10.0.0.103 -c2
