c082fb7df4e5a55c37f96d5a85d1cf475be521a8
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-05 11:47:21 -0500

    Update

--- src/static-vxlan-agent/test/check_grpc_dependencies.py
+++ src/static-vxlan-agent/test/check_grpc_dependencies.py
@@ -3,7 +3,8 @@
 
 # Small test program to check which grpc files are loaded for eventlet support
 # Usage:
-# source /opt/static-vxlan-agent/.venv/bin/activate
+# export VIRTUAL_ENV=/opt/static-vxlan-agent/.venv/
+# export PATH=${VIRTUAL_ENV}/bin:${PATH}
 # PYTHONVERBOSE=2 python3 check_grpc_dependencies.py 2>&1 | grep import
 
 # Google core libraries don't support eventlet; workaround
